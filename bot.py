11a4e0ab8c536300ab1826ad1c18992478c9b04f
yuskey <heredia.brian@gmail.com>
2018-01-10 20:51:57 -0500

    Added downloading image-attachments and outline for NN idea

--- bot.py
+++ bot.py
@@ -3,6 +3,12 @@ import asyncio
 from discord.ext import commands
 from discord.ext.commands import Bot
 import yaml
+
+#for my neural network work
+import tensorflow as tf
+import numpy as np
+from requests import get
+
 # debugger
 # use `embed()` to breakpoint
 from IPython import embed
@@ -25,6 +31,7 @@ class discord_bot(object):
             if reaction.emoji == '🗑' and reaction.message.author == bot.user and user != bot.user:
                 await bot.delete_message(reaction.message)
 
+        #bot delete command test
         @bot.command(pass_context = True)
         async def delete_me(ctx):
             # remove user's command message
@@ -33,12 +40,49 @@ class discord_bot(object):
             test_message = await bot.say("Hello World??")
             await bot.add_reaction(test_message, '🗑')
 
+         #Basic ping pong test
         @bot.command(pass_context = True)
         async def ping(*args):
             message = await bot.say(":ping_pong: Pong!")
             await bot.add_reaction(message, '🗑')
 
+        #Saves whatever picture is attached with the message, as the file name
+        @bot.command(pass_context = True)
+        async def save_meme(ctx):
+            file_name = ctx.message.content[11:] + ".jpg"
+            self.download_image(ctx.message.attachments[0]['url'], file_name)
+            await bot.delete_message(ctx.message)
+            message = await bot.say("Saved Meme to %s" %file_name)
+            await bot.add_reaction(message, '🗑')
+
+        """Gonna work on trying to get an image recognition method working for fun
+        command is gonna be !identify
+        will take in an input image and run it through a classifier and see attempt to identify image
+        Image dimensions: tbd
+        Neural network implemented in tensorflow"""
+
+        @bot.command(pass_context = True)
+        async def identify(ctx):
+            print(ctx.message.attachments[0]['url'])
+            self.download_image(ctx.message.attachments[0]['url'], "test.jpg")
+            message = await bot.say(ctx.message.attachments[0]['url'])
+            await bot.add_reaction(message, '🗑')
+
         bot.run(token)
+
+    def download_image(self, url, save_file):
+        with open(save_file, 'wb') as sfile:
+            response = get(url)
+            sfile.write(response.content)
+
+
+
+    #def identify_image(self, image):
+
+    #def neural_net(self):
+
+
+
 
 
 def main():
@@ -46,6 +90,7 @@ def main():
     secret_path = "C:/Users/Brian/Documents/GitHub/secrets.yml"
     secrets = yaml.load(open(secret_path, 'r'))
     bot = discord_bot(secrets['BOT_TOKEN'])
+
 
 if __name__ == "__main__":
     main()
